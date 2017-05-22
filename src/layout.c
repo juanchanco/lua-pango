c341515fcb1ba9999123b36894cea31032336439
Juan Chanco <juan.chanco@bluestembrands.com>
2017-05-22 14:39:45 -0500

    filling out object structure

--- src/layout.c
+++ src/layout.c
@@ -7,6 +7,8 @@
 #include "common/common.h"
 #include "common/table.h"
 
+#include "layout.h"
+
 const CommonEnum LuaPangoWrapMode[] = {
     { "Word", PANGO_WRAP_WORD },
     { "Char", PANGO_WRAP_CHAR },
@@ -30,106 +32,144 @@ const CommonEnum LuaPangoAlignment[] = {
 };
 
 
-/*PangoLayout * 	pango_layout_new ()*/
-/*PangoLayout * 	pango_layout_copy ()*/
-/*PangoContext * 	pango_layout_get_context ()*/
-/*void 	pango_layout_context_changed ()*/
-/*guint 	pango_layout_get_serial ()*/
+/*PangoLayout *  pango_layout_new ()*/
+/*PangoLayout *  pango_layout_copy ()*/
+/*PangoContext *  pango_layout_get_context ()*/
+/*void  pango_layout_context_changed ()*/
+/*guint  pango_layout_get_serial ()*/
 static int _pango_layout_set_text (lua_State* L) {
-    PangoLayout *layout = commonGetAs(L, 1, "Layout", PangoLayout *);
+    PangoLayout *layout = commonGetAs(L, 1, Layout.name, PangoLayout *);
     const char* text = luaL_checkstring(L, 2);
     int length = luaL_checkinteger(L, 3);
     pango_layout_set_text(layout, text, length);
     return commonPush(L, "b", 1);
 }
-/*const char * 	pango_layout_get_text ()*/
-/*gint 	pango_layout_get_character_count ()*/
-/*void 	pango_layout_set_markup ()*/
-/*void 	pango_layout_set_markup_with_accel ()*/
-/*void 	pango_layout_set_attributes ()*/
-/*PangoAttrList * 	pango_layout_get_attributes ()*/
+/*const char *  pango_layout_get_text ()*/
+/*gint  pango_layout_get_character_count ()*/
+/*void  pango_layout_set_markup ()*/
+/*void  pango_layout_set_markup_with_accel ()*/
+/*void  pango_layout_set_attributes ()*/
+/*PangoAttrList *  pango_layout_get_attributes ()*/
 static int _pango_layout_set_font_description (lua_State* L) {
-    PangoLayout *layout = commonGetAs(L, 1, "Layout", PangoLayout *);
+    PangoLayout *layout = commonGetAs(L, 1, Layout.name, PangoLayout *);
     PangoFontDescription *desc =
         commonGetAs(L, 2, "FontDescription", PangoFontDescription *);
     pango_layout_set_font_description(layout, desc);
     return commonPush(L, "b", 1);
 }
-/*const PangoFontDescription * 	pango_layout_get_font_description ()*/
-/*void 	pango_layout_set_width ()*/
-/*int 	pango_layout_get_width ()*/
-/*void 	pango_layout_set_height ()*/
-/*int 	pango_layout_get_height ()*/
-/*void 	pango_layout_set_wrap ()*/
-/*PangoWrapMode 	pango_layout_get_wrap ()*/
-/*gboolean 	pango_layout_is_wrapped ()*/
-/*void 	pango_layout_set_ellipsize ()*/
-/*PangoEllipsizeMode 	pango_layout_get_ellipsize ()*/
-/*gboolean 	pango_layout_is_ellipsized ()*/
-/*void 	pango_layout_set_indent ()*/
-/*int 	pango_layout_get_indent ()*/
-/*int 	pango_layout_get_spacing ()*/
-/*void 	pango_layout_set_spacing ()*/
-/*void 	pango_layout_set_justify ()*/
-/*gboolean 	pango_layout_get_justify ()*/
-/*void 	pango_layout_set_auto_dir ()*/
-/*gboolean 	pango_layout_get_auto_dir ()*/
-/*void 	pango_layout_set_alignment ()*/
-/*PangoAlignment 	pango_layout_get_alignment ()*/
-/*void 	pango_layout_set_tabs ()*/
-/*PangoTabArray * 	pango_layout_get_tabs ()*/
-/*void 	pango_layout_set_single_paragraph_mode ()*/
-/*gboolean 	pango_layout_get_single_paragraph_mode ()*/
-/*int 	pango_layout_get_unknown_glyphs_count ()*/
-/*void 	pango_layout_get_log_attrs ()*/
-/*const PangoLogAttr * 	pango_layout_get_log_attrs_readonly ()*/
-/*void 	pango_layout_index_to_pos ()*/
-/*void 	pango_layout_index_to_line_x ()*/
-/*gboolean 	pango_layout_xy_to_index ()*/
-/*void 	pango_layout_get_cursor_pos ()*/
-/*void 	pango_layout_move_cursor_visually ()*/
-/*void 	pango_layout_get_extents ()*/
-/*void 	pango_layout_get_pixel_extents ()*/
+/*const PangoFontDescription *  pango_layout_get_font_description ()*/
+/*void  pango_layout_set_width ()*/
+/*int  pango_layout_get_width ()*/
+/*void  pango_layout_set_height ()*/
+/*int  pango_layout_get_height ()*/
+/*void  pango_layout_set_wrap ()*/
+/*PangoWrapMode  pango_layout_get_wrap ()*/
+/*gboolean  pango_layout_is_wrapped ()*/
+/*void  pango_layout_set_ellipsize ()*/
+/*PangoEllipsizeMode  pango_layout_get_ellipsize ()*/
+/*gboolean  pango_layout_is_ellipsized ()*/
+/*void  pango_layout_set_indent ()*/
+/*int  pango_layout_get_indent ()*/
+/*int  pango_layout_get_spacing ()*/
+/*void  pango_layout_set_spacing ()*/
+/*void  pango_layout_set_justify ()*/
+/*gboolean  pango_layout_get_justify ()*/
+/*void  pango_layout_set_auto_dir ()*/
+/*gboolean  pango_layout_get_auto_dir ()*/
+/*void  pango_layout_set_alignment ()*/
+/*PangoAlignment  pango_layout_get_alignment ()*/
+/*void  pango_layout_set_tabs ()*/
+/*PangoTabArray *  pango_layout_get_tabs ()*/
+/*void  pango_layout_set_single_paragraph_mode ()*/
+/*gboolean  pango_layout_get_single_paragraph_mode ()*/
+/*int  pango_layout_get_unknown_glyphs_count ()*/
+/*void  pango_layout_get_log_attrs ()*/
+/*const PangoLogAttr *  pango_layout_get_log_attrs_readonly ()*/
+/*void  pango_layout_index_to_pos ()*/
+/*void  pango_layout_index_to_line_x ()*/
+/*gboolean  pango_layout_xy_to_index ()*/
+/*void  pango_layout_get_cursor_pos ()*/
+/*void  pango_layout_move_cursor_visually ()*/
+/*void  pango_layout_get_extents ()*/
+/*void  pango_layout_get_pixel_extents ()*/
 static int _pango_layout_get_size (lua_State* L) {
-    PangoLayout *layout = commonGetAs(L, 1, "Layout", PangoLayout *);
-    int *width = (int *) 0 ;
-    int *height = (int *) 0 ;
+    PangoLayout *layout = commonGetAs(L, 1, Layout.name, PangoLayout *);
+    int *width = (int *) 0;
+    int *height = (int *) 0;
     pango_layout_get_size(layout, width, height);
     return commonPush(L, "ii", width, height);
 }
-/*void 	pango_layout_get_pixel_size ()*/
-/*int 	pango_layout_get_baseline ()*/
-/*int 	pango_layout_get_line_count ()*/
-/*PangoLayoutLine * 	pango_layout_get_line ()*/
-/*PangoLayoutLine * 	pango_layout_get_line_readonly ()*/
-/*GSList * 	pango_layout_get_lines ()*/
-/*GSList * 	pango_layout_get_lines_readonly ()*/
-/*PangoLayoutIter * 	pango_layout_get_iter ()*/
-/*PangoLayoutIter * 	pango_layout_iter_copy ()*/
-/*void 	pango_layout_iter_free ()*/
-/*gboolean 	pango_layout_iter_next_run ()*/
-/*gboolean 	pango_layout_iter_next_char ()*/
-/*gboolean 	pango_layout_iter_next_cluster ()*/
-/*gboolean 	pango_layout_iter_next_line ()*/
-/*gboolean 	pango_layout_iter_at_last_line ()*/
-/*int 	pango_layout_iter_get_index ()*/
-/*int 	pango_layout_iter_get_baseline ()*/
-/*PangoLayoutRun * 	pango_layout_iter_get_run ()*/
-/*PangoLayoutRun * 	pango_layout_iter_get_run_readonly ()*/
-/*PangoLayoutLine * 	pango_layout_iter_get_line ()*/
-/*PangoLayoutLine * 	pango_layout_iter_get_line_readonly ()*/
-/*PangoLayout * 	pango_layout_iter_get_layout ()*/
-/*void 	pango_layout_iter_get_char_extents ()*/
-/*void 	pango_layout_iter_get_cluster_extents ()*/
-/*void 	pango_layout_iter_get_run_extents ()*/
-/*void 	pango_layout_iter_get_line_yrange ()*/
-/*void 	pango_layout_iter_get_line_extents ()*/
-/*void 	pango_layout_iter_get_layout_extents ()*/
-/*PangoLayoutLine * 	pango_layout_line_ref ()*/
-/*void 	pango_layout_line_unref ()*/
-/*void 	pango_layout_line_get_extents ()*/
-/*void 	pango_layout_line_get_pixel_extents ()*/
-/*void 	pango_layout_line_index_to_x ()*/
-/*gboolean 	pango_layout_line_x_to_index ()*/
-/*void 	pango_layout_line_get_x_ranges ()*/
+/*void  pango_layout_get_pixel_size ()*/
+/*int  pango_layout_get_baseline ()*/
+/*int  pango_layout_get_line_count ()*/
+/*PangoLayoutLine *  pango_layout_get_line ()*/
+/*PangoLayoutLine *  pango_layout_get_line_readonly ()*/
+/*GSList *  pango_layout_get_lines ()*/
+/*GSList *  pango_layout_get_lines_readonly ()*/
+/*PangoLayoutIter *  pango_layout_get_iter ()*/
+/*PangoLayoutIter *  pango_layout_iter_copy ()*/
+/*void  pango_layout_iter_free ()*/
+/*gboolean  pango_layout_iter_next_run ()*/
+/*gboolean  pango_layout_iter_next_char ()*/
+/*gboolean  pango_layout_iter_next_cluster ()*/
+/*gboolean  pango_layout_iter_next_line ()*/
+/*gboolean  pango_layout_iter_at_last_line ()*/
+/*int  pango_layout_iter_get_index ()*/
+/*int  pango_layout_iter_get_baseline ()*/
+/*PangoLayoutRun *  pango_layout_iter_get_run ()*/
+/*PangoLayoutRun *  pango_layout_iter_get_run_readonly ()*/
+/*PangoLayoutLine *  pango_layout_iter_get_line ()*/
+/*PangoLayoutLine *  pango_layout_iter_get_line_readonly ()*/
+/*PangoLayout *  pango_layout_iter_get_layout ()*/
+/*void  pango_layout_iter_get_char_extents ()*/
+/*void  pango_layout_iter_get_cluster_extents ()*/
+/*void  pango_layout_iter_get_run_extents ()*/
+/*void  pango_layout_iter_get_line_yrange ()*/
+/*void  pango_layout_iter_get_line_extents ()*/
+/*void  pango_layout_iter_get_layout_extents ()*/
+/*PangoLayoutLine *  pango_layout_line_ref ()*/
+/*void  pango_layout_line_unref ()*/
+/*void  pango_layout_line_get_extents ()*/
+/*void  pango_layout_line_get_pixel_extents ()*/
+/*void  pango_layout_line_index_to_x ()*/
+/*gboolean  pango_layout_line_x_to_index ()*/
+/*void  pango_layout_line_get_x_ranges ()*/
 
+static int l_texture_gc(lua_State *L) {
+CommonUserdata *udata = commonGetUserdata(L, 1, LayoutName);
+
+if (udata->mustdelete)
+	/*TODO: count down to zero?*/
+	g_object_unref(udata->data);
+
+return 0;
+
+/*PangoLayout *Layout = commonGetUserdata(L, 1, LayoutName);*/
+/*g_object_unref(layout);*/
+return 0;
+}
+
+
+/* --------------------------------------------------------
+ * Layout object definition
+ * -------------------------------------------------------- */
+
+const luaL_Reg LayoutMethods[] = {
+{ "setText", _pango_layout_set_text },
+{ "setFontDescription", _pango_layout_set_font_description },
+{ "getSize", _pango_layout_get_size },
+{ NULL, NULL }
+};
+
+const luaL_Reg LayoutMetamethods[] = {
+ /*{ "__eq", l_texture_eq },*/
+ { "__gc", l_texture_gc },
+ /*{ "__tostring", l_texture_tostring },*/
+ { NULL, NULL }
+};
+
+const CommonObject Layout = {
+"Layout",
+LayoutMethods,
+LayoutMetamethods
+};
---
+++ src/layout.h
@@ -0,0 +1,12 @@
+#ifndef _LAYOUT_H_
+#define _LAYOUT_H_
+
+#include <common/common.h>
+
+#define LayoutName	Layout.name
+
+extern const luaL_Reg LayoutFunctions[];
+
+extern const CommonObject Layout;
+
+#endif /* !_LAYOUT_H_ */
