0f7b9162a84d0b486f7487621e5090341d4c7b35
Juan Chanco <juan.chanco@bluestembrands.com>
2017-05-22 13:04:33 -0500

    skeletons. glib

---
+++ src/font_desc.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <config.h>
+
+#include <pango/pango.h>
+#include "common/common.h"
+#include "common/table.h"
+
+const CommonEnum LuaPangoStyle[] = {
+    { "Normal", PANGO_STYLE_NORMAL },
+    { "Oblique", PANGO_STYLE_OBLIQUE },
+    { "Italic", PANGO_STYLE_ITALIC },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoWeight[] = {
+    { "Thin", PANGO_WEIGHT_THIN },
+    { "Ultralight", PANGO_WEIGHT_ULTRALIGHT },
+    { "Light", PANGO_WEIGHT_LIGHT },
+    { "Semilight", PANGO_WEIGHT_SEMILIGHT },
+    { "Book", PANGO_WEIGHT_BOOK },
+    { "Normal", PANGO_WEIGHT_NORMAL },
+    { "Medium", PANGO_WEIGHT_MEDIUM },
+    { "Semibold", PANGO_WEIGHT_SEMIBOLD },
+    { "Bold", PANGO_WEIGHT_BOLD },
+    { "Ultrabold", PANGO_WEIGHT_ULTRABOLD },
+    { "Heavy", PANGO_WEIGHT_HEAVY },
+    { "Ultraheavy", PANGO_WEIGHT_ULTRAHEAVY },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoVariant[] = {
+    { "Normal", PANGO_VARIANT_NORMAL },
+    { "Small_caps", PANGO_VARIANT_SMALL_CAPS },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoStretch[] = {
+    { "Ultra_condensed", PANGO_STRETCH_ULTRA_CONDENSED },
+    { "Extra_condensed", PANGO_STRETCH_EXTRA_CONDENSED },
+    { "Condensed", PANGO_STRETCH_CONDENSED },
+    { "Semi_condensed", PANGO_STRETCH_SEMI_CONDENSED },
+    { "Normal", PANGO_STRETCH_NORMAL },
+    { "Semi_expanded", PANGO_STRETCH_SEMI_EXPANDED },
+    { "Expanded", PANGO_STRETCH_EXPANDED },
+    { "Extra_expanded", PANGO_STRETCH_EXTRA_EXPANDED },
+    { "Ultra_expanded", PANGO_STRETCH_ULTRA_EXPANDED },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoFontMask[] = {
+    { "Family", PANGO_FONT_MASK_FAMILY },
+    { "Style", PANGO_FONT_MASK_STYLE },
+    { "Variant", PANGO_FONT_MASK_VARIANT },
+    { "Weight", PANGO_FONT_MASK_WEIGHT },
+    { "Stretch", PANGO_FONT_MASK_STRETCH },
+    { "Size", PANGO_FONT_MASK_SIZE },
+    { "Gravity", PANGO_FONT_MASK_GRAVITY },
+    { NULL, -1 }
+};
+
+
+/*PangoFontDescription * 	pango_font_description_new ()*/
+/*PangoFontDescription * 	pango_font_description_copy ()*/
+/*PangoFontDescription * 	pango_font_description_copy_static ()*/
+/*guint 	pango_font_description_hash ()*/
+/*gboolean 	pango_font_description_equal ()*/
+void _pango_font_description_free () {}
+/*void 	pango_font_descriptions_free ()*/
+/*void 	pango_font_description_set_family ()*/
+/*void 	pango_font_description_set_family_static ()*/
+/*const char * 	pango_font_description_get_family ()*/
+/*void 	pango_font_description_set_style ()*/
+/*PangoStyle 	pango_font_description_get_style ()*/
+/*void 	pango_font_description_set_variant ()*/
+/*PangoVariant 	pango_font_description_get_variant ()*/
+/*void 	pango_font_description_set_weight ()*/
+/*PangoWeight 	pango_font_description_get_weight ()*/
+/*void 	pango_font_description_set_stretch ()*/
+/*PangoStretch 	pango_font_description_get_stretch ()*/
+/*void 	pango_font_description_set_size ()*/
+/*gint 	pango_font_description_get_size ()*/
+void _pango_font_description_set_absolute_size () {}
+/*gboolean 	pango_font_description_get_size_is_absolute ()*/
+/*void 	pango_font_description_set_gravity ()*/
+/*PangoGravity 	pango_font_description_get_gravity ()*/
+/*PangoFontMask 	pango_font_description_get_set_fields ()*/
+/*void 	pango_font_description_unset_fields ()*/
+/*void 	pango_font_description_merge ()*/
+/*void 	pango_font_description_merge_static ()*/
+/*gboolean 	pango_font_description_better_match ()*/
+PangoFontDescription * _pango_font_description_from_string () {}
+/*char * 	pango_font_description_to_string ()*/
+/*char * 	pango_font_description_to_filename ()*/
+/*PangoFontMetrics * 	pango_font_metrics_ref ()*/
+/*void 	pango_font_metrics_unref ()*/
+/*int 	pango_font_metrics_get_ascent ()*/
+/*int 	pango_font_metrics_get_descent ()*/
+/*int 	pango_font_metrics_get_approximate_char_width ()*/
+/*int 	pango_font_metrics_get_approximate_digit_width ()*/
+/*int 	pango_font_metrics_get_underline_thickness ()*/
+/*int 	pango_font_metrics_get_underline_position ()*/
+/*int 	pango_font_metrics_get_strikethrough_thickness ()*/
+/*int 	pango_font_metrics_get_strikethrough_position ()*/
+/*#define 	PANGO_FONT()*/
+/*#define 	PANGO_IS_FONT()*/
+/*PangoEngineShape * 	pango_font_find_shaper ()*/
+/*PangoFontDescription * 	pango_font_describe ()*/
+/*PangoFontDescription * 	pango_font_describe_with_absolute_size ()*/
+/*PangoCoverage * 	pango_font_get_coverage ()*/
+/*void 	pango_font_get_glyph_extents ()*/
+/*PangoFontMetrics * 	pango_font_get_metrics ()*/
+/*PangoFontMap * 	pango_font_get_font_map ()*/
+/*#define 	PANGO_FONT_FAMILY()*/
+/*#define 	PANGO_IS_FONT_FAMILY()*/
+/*const char * 	pango_font_family_get_name ()*/
+/*gboolean 	pango_font_family_is_monospace ()*/
+/*void 	pango_font_family_list_faces ()*/
+/*#define 	PANGO_FONT_FACE()*/
+/*#define 	PANGO_IS_FONT_FACE()*/
+/*const char * 	pango_font_face_get_face_name ()*/
+/*void 	pango_font_face_list_sizes ()*/
+/*PangoFontDescription * 	pango_font_face_describe ()*/
+/*gboolean 	pango_font_face_is_synthesized ()*/
+/*#define 	PANGO_FONT_MAP()*/
+/*#define 	PANGO_IS_FONT_MAP()*/
+/*#define 	PANGO_FONT_MAP_CLASS()*/
+/*#define 	PANGO_IS_FONT_MAP_CLASS()*/
+/*#define 	PANGO_FONT_MAP_GET_CLASS()*/
+/*PangoContext * 	pango_font_map_create_context ()*/
+/*PangoFont * 	pango_font_map_load_font ()*/
+/*PangoFontset * 	pango_font_map_load_fontset ()*/
+/*void 	pango_font_map_list_families ()*/
+/*const char * 	pango_font_map_get_shape_engine_type ()*/
+/*guint 	pango_font_map_get_serial ()*/
+/*void 	pango_font_map_changed ()*/
+/*PangoFont * 	pango_fontset_get_font ()*/
+/*PangoFontMetrics * 	pango_fontset_get_metrics ()*/
+/*gboolean 	(*PangoFontsetForeachFunc) ()*/
+/*void 	pango_fontset_foreach ()*/
+/*PangoFontsetSimple * 	pango_fontset_simple_new ()*/
+/*void 	pango_fontset_simple_append ()*/
+/*int 	pango_fontset_simple_size ()*/
+
---
+++ src/layout.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <config.h>
+
+#include <pango/pango.h>
+#include "common/common.h"
+#include "common/table.h"
+
+const CommonEnum LuaPangoWrapMode[] = {
+    { "Word", PANGO_WRAP_WORD },
+    { "Char", PANGO_WRAP_CHAR },
+    { "Word_char", PANGO_WRAP_WORD_CHAR },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoEllipsizeMode[] = {
+    { "None", PANGO_ELLIPSIZE_NONE },
+    { "Start", PANGO_ELLIPSIZE_START },
+    { "Middle", PANGO_ELLIPSIZE_MIDDLE },
+    { "End", PANGO_ELLIPSIZE_END },
+    { NULL, -1 }
+};
+
+const CommonEnum LuaPangoAlignment[] = {
+    { "Left", PANGO_ALIGN_LEFT },
+    { "Center", PANGO_ALIGN_CENTER },
+    { "Right", PANGO_ALIGN_RIGHT },
+    { NULL, -1 }
+};
+
+
+/*PangoLayout * 	pango_layout_new ()*/
+/*PangoLayout * 	pango_layout_copy ()*/
+/*PangoContext * 	pango_layout_get_context ()*/
+/*void 	pango_layout_context_changed ()*/
+/*guint 	pango_layout_get_serial ()*/
+void 	_pango_layout_set_text () {}
+/*const char * 	pango_layout_get_text ()*/
+/*gint 	pango_layout_get_character_count ()*/
+/*void 	pango_layout_set_markup ()*/
+/*void 	pango_layout_set_markup_with_accel ()*/
+/*void 	pango_layout_set_attributes ()*/
+/*PangoAttrList * 	pango_layout_get_attributes ()*/
+void 	_pango_layout_set_font_description () {}
+/*const PangoFontDescription * 	pango_layout_get_font_description ()*/
+/*void 	pango_layout_set_width ()*/
+/*int 	pango_layout_get_width ()*/
+/*void 	pango_layout_set_height ()*/
+/*int 	pango_layout_get_height ()*/
+/*void 	pango_layout_set_wrap ()*/
+/*PangoWrapMode 	pango_layout_get_wrap ()*/
+/*gboolean 	pango_layout_is_wrapped ()*/
+/*void 	pango_layout_set_ellipsize ()*/
+/*PangoEllipsizeMode 	pango_layout_get_ellipsize ()*/
+/*gboolean 	pango_layout_is_ellipsized ()*/
+/*void 	pango_layout_set_indent ()*/
+/*int 	pango_layout_get_indent ()*/
+/*int 	pango_layout_get_spacing ()*/
+/*void 	pango_layout_set_spacing ()*/
+/*void 	pango_layout_set_justify ()*/
+/*gboolean 	pango_layout_get_justify ()*/
+/*void 	pango_layout_set_auto_dir ()*/
+/*gboolean 	pango_layout_get_auto_dir ()*/
+/*void 	pango_layout_set_alignment ()*/
+/*PangoAlignment 	pango_layout_get_alignment ()*/
+/*void 	pango_layout_set_tabs ()*/
+/*PangoTabArray * 	pango_layout_get_tabs ()*/
+/*void 	pango_layout_set_single_paragraph_mode ()*/
+/*gboolean 	pango_layout_get_single_paragraph_mode ()*/
+/*int 	pango_layout_get_unknown_glyphs_count ()*/
+/*void 	pango_layout_get_log_attrs ()*/
+/*const PangoLogAttr * 	pango_layout_get_log_attrs_readonly ()*/
+/*void 	pango_layout_index_to_pos ()*/
+/*void 	pango_layout_index_to_line_x ()*/
+/*gboolean 	pango_layout_xy_to_index ()*/
+/*void 	pango_layout_get_cursor_pos ()*/
+/*void 	pango_layout_move_cursor_visually ()*/
+/*void 	pango_layout_get_extents ()*/
+/*void 	pango_layout_get_pixel_extents ()*/
+void 	_pango_layout_get_size () {}
+/*void 	pango_layout_get_pixel_size ()*/
+/*int 	pango_layout_get_baseline ()*/
+/*int 	pango_layout_get_line_count ()*/
+/*PangoLayoutLine * 	pango_layout_get_line ()*/
+/*PangoLayoutLine * 	pango_layout_get_line_readonly ()*/
+/*GSList * 	pango_layout_get_lines ()*/
+/*GSList * 	pango_layout_get_lines_readonly ()*/
+/*PangoLayoutIter * 	pango_layout_get_iter ()*/
+/*PangoLayoutIter * 	pango_layout_iter_copy ()*/
+/*void 	pango_layout_iter_free ()*/
+/*gboolean 	pango_layout_iter_next_run ()*/
+/*gboolean 	pango_layout_iter_next_char ()*/
+/*gboolean 	pango_layout_iter_next_cluster ()*/
+/*gboolean 	pango_layout_iter_next_line ()*/
+/*gboolean 	pango_layout_iter_at_last_line ()*/
+/*int 	pango_layout_iter_get_index ()*/
+/*int 	pango_layout_iter_get_baseline ()*/
+/*PangoLayoutRun * 	pango_layout_iter_get_run ()*/
+/*PangoLayoutRun * 	pango_layout_iter_get_run_readonly ()*/
+/*PangoLayoutLine * 	pango_layout_iter_get_line ()*/
+/*PangoLayoutLine * 	pango_layout_iter_get_line_readonly ()*/
+/*PangoLayout * 	pango_layout_iter_get_layout ()*/
+/*void 	pango_layout_iter_get_char_extents ()*/
+/*void 	pango_layout_iter_get_cluster_extents ()*/
+/*void 	pango_layout_iter_get_run_extents ()*/
+/*void 	pango_layout_iter_get_line_yrange ()*/
+/*void 	pango_layout_iter_get_line_extents ()*/
+/*void 	pango_layout_iter_get_layout_extents ()*/
+/*PangoLayoutLine * 	pango_layout_line_ref ()*/
+/*void 	pango_layout_line_unref ()*/
+/*void 	pango_layout_line_get_extents ()*/
+/*void 	pango_layout_line_get_pixel_extents ()*/
+/*void 	pango_layout_line_index_to_x ()*/
+/*gboolean 	pango_layout_line_x_to_index ()*/
+/*void 	pango_layout_line_get_x_ranges ()*/
+
