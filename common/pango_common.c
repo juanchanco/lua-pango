c8625c80c99f1b2b7fb7448e9e2b02f25505ed6d
Juan Chanco <juan.chanco@bluestembrands.com>
2017-06-13 17:10:25 -0500

    small fix

--- common/pango_common.c
+++ common/pango_common.c
@@ -10,11 +10,9 @@
 void
 pushRect(lua_State *L, const PangoRectangle *rect)
 {
-	int i;
-
 	lua_createtable(L, 0, 0);
     tableSetInt(L, -1, "x", rect->x);
     tableSetInt(L, -1, "y", rect->y);
     tableSetInt(L, -1, "width", rect->width);
-    tableSetInt(L, -1, "height", rect->x);
+    tableSetInt(L, -1, "height", rect->height);
 }
